89bc906f607a5806f0b04535ca288a1a65670245
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-05 01:34:05 +0100

    try custom handlers

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,8 +5,9 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/api/random-uuid.ts";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -14,8 +15,9 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/api/random-uuid.ts": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
