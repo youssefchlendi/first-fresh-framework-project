21306a4e1dea887e1b314366b255c1bacf0b33ad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-05 01:35:45 +0100

    try fetching data

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,8 +6,9 @@ import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/random-uuid.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
+import * as $4 from "./routes/github/[username].tsx";
+import * as $5 from "./routes/greet/[name].tsx";
+import * as $6 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -16,8 +17,9 @@ const manifest = {
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/api/random-uuid.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
+    "./routes/github/[username].tsx": $4,
+    "./routes/greet/[name].tsx": $5,
+    "./routes/index.tsx": $6,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
