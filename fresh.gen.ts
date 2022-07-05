3914dd45eec2c333dabdd3569523f7909b098d41
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-05 01:31:33 +0100

    try dynamic routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,8 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/greet/[name].tsx";
+import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -13,7 +14,8 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/greet/[name].tsx": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/greet/[name].tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from "preact";
+import { PageProps } from "$fresh/server.ts";
+
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+  return (
+    <main>
+      <p>Greetings to you, {name}!</p>
+    </main>
+  );
+}
